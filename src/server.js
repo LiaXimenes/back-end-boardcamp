111ed7c9d2d92654fa09d7102fa17d80369e8fac
Lia Ximenes <ximeneslia@gmail.com>
2021-06-17 22:46:10 -0300

    incilização do alugueis

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import pg from 'pg';
+import Joi from 'joi';
+import dayjs from 'dayjs';
+
 
 const { Pool } = pg;
 
@@ -114,27 +117,31 @@ server.post("/customers", async (req, res) =>{
     const cpf = parseInt(req.body.cpf);
     const birthday = req.body.birthday;
 
+    const schema = Joi.object({
+        name: Joi.string().required(),
+        cpf: Joi.string().length(11).required(),
+        phone: Joi.string().min(10).max(11).required(),
+        birthday: Joi.date().less('now').required(),
+    })
 
-    try{
-        const isCpfUsed = await connection.query('SELECT cpf FROM customers WHERE cpf = $1', [cpf]);
-        if(isCpfUsed.rows.length !== 0){
-            return res.sendStatus(409);
+    const validation = schema.validate(req.body)
+
+    if(!validation.error){
+        try{
+            const isCpfUsed = await connection.query('SELECT cpf FROM customers WHERE cpf = $1', [cpf]);
+            if(isCpfUsed.rows.length !== 0){
+                return res.sendStatus(409);
+            }
+          
+            await connection.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)', [name, phone, cpf, birthday]);
+            res.sendStatus(201);
+    
+        } catch(err){
+            console.log(err);
         }
-
-        if(name === ""){
-            return res.sendStatus(400);
-        }
-
-        // `cpf` deve ser uma string com 11 caracteres numéricos;
-        // `phone` deve ser uma string com 10 ou 11 caracteres numéricos; 
-        // `birthday` deve ser uma data válida;
-
-        await connection.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)', [name, phone, cpf, birthday]);
-        res.sendStatus(201);
-
-    } catch(err){
-        console.log(err);
-    }
+    } else{
+        res.sendStatus(400);
+    }   
 })
 
 server.put("/customers/:id", async (req, res) =>{
@@ -144,21 +151,69 @@ server.put("/customers/:id", async (req, res) =>{
     const cpf = parseInt(req.body.cpf);
     const birthday = req.body.birthday;
 
-   try{
-        const isCpfUsed = await connection.query('SELECT cpf FROM customers WHERE cpf = $1', [cpf]);
-        if(isCpfUsed.rows.length !== 0){
-            return res.sendStatus(409);
+    const schema = Joi.object({
+        name: Joi.string().name().required(),
+        cpf: Joi.string().length(11).required(),
+        phone: Joi.string().min(10).max(11).required(),
+        birthday: Joi.date().less('now').required(),
+    })
+
+    const validation = schema.validate(req.body)
+
+    if(!validation.error){
+        try{
+            const isCpfUsed = await connection.query('SELECT cpf FROM customers WHERE cpf = $1', [cpf]);
+            if(isCpfUsed.rows.length !== 0){
+                return res.sendStatus(409);
+            }
+
+            await connection.query('UPDATE customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)', [name, phone, cpf, birthday]);
+            res.sendStatus(200);
+
+        } catch(err){
+            console.log(err);
         }
+    } else{
+        res.sendStatus(400);
 
-        if(name === ""){
-            return res.sendStatus(400);
-        }
+    }
+})
 
-        // `cpf` deve ser uma string com 11 caracteres numéricos;
-        // `phone` deve ser uma string com 10 ou 11 caracteres numéricos; 
-        // `birthday` deve ser uma data válida;
+server.get("/rentals", async (req, res) =>{
 
-        await connection.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)', [name, phone, cpf, birthday]);
+    try{
+        const rentals =  await connection.query('SELECT * FROM rentals');
+        res.send(rentals.rows);
+    } catch(err){
+        console.log(err);
+    }
+    
+})
+
+server.post("/rentals", async (req, res) =>{
+    const {customerId, gameId, daysRented } = req.body
+    const rentDate = dayjs();
+    const originalPrice = (15000*daysRented);
+    const returnDate = null;
+    const delayFee = null;
+    
+
+    const isCustomerExistent = await connection.query('SELECT id FROM customers WHERE id = $1', [customerId]);
+    if(isCustomerExistent.rows.length === 0){
+        return res.sendStatus(400);
+    }
+
+    const isGameExistent = await connection.query('SELECT id FROM games WHERE id = $1', [gameId]);
+    if(isGameExistent.rows.length === 0){
+        return res.sendStatus(400);
+    }
+
+    if(daysRented < 1 ){
+        return res.sendStatus(400);
+    }
+
+    try{    
+        await connection.query('INSERT INTO rentals ("customerId", "gameId", "daysRented", "rentDate", "originalPrice", "returnDate", "delayFee") VALUES ($1, $2, $3, $4, $5, $6, $7)', [customerId, gameId, daysRented, rentDate, originalPrice, returnDate, delayFee]);
         res.sendStatus(201);
 
     } catch(err){
@@ -166,6 +221,50 @@ server.put("/customers/:id", async (req, res) =>{
     }
 })
 
+server.post("/rentals/:id/return", async (req,res) =>{
+    const id = req.params.id;
+    const returnDate = dayjs();
+
+    const isRentalExistent = await connection.query('SELECT id FROM rentals WHERE id = $1', [id]);
+    if(isRentalExistent.rows.length === 0){
+        return res.sendStatus(404);
+    }
+
+    const paidRental = await connection.query('SELECT * FROM rentals WHERE id = $1', [id]);
+    if(paidRental.rows.returnDate !== null){
+        return res.sendStatus(400);
+    }
+
+    try{    
+        await connection.query(`UPDATE rentals SET "returnDate" = $1 WHERE id = $2`, [returnDate, id]);
+        res.sendStatus(201);
+
+    } catch(err){
+        console.log(err);
+    }
+})
+
+server.delete("/rentals/:id", async (req, res) =>{
+    const id = req.params.id;
+
+    const isRentalExistent = await connection.query('SELECT id FROM rentals WHERE id = $1', [id]);
+    if(isRentalExistent.rows.length === 0){
+        return res.sendStatus(404);
+    }
+
+    const paidRental = await connection.query('SELECT * FROM rentals WHERE id = $1', [id]);
+    if(paidRental.rows.returnDate == null){
+        return res.sendStatus(400);
+    }
+
+    try{    
+        await connection.query('DELETE FROM rentals WHERE id = $1',[id]);
+        res.sendStatus(201);
+
+    } catch(err){
+        console.log(err);
+    }
+})
 
 server.listen(4000, () => {
   console.log("server listening at 4000 ");
